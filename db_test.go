e82c4704c647af91b92bdacb2458d0bd3151af9c
Matheus Mina <mfbmina@gmail.com>
2025-07-23 09:28:08 -0300

    initial commit

---
+++ db_test.go
@@ -0,0 +1,67 @@
+package poctestcontainers
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	_ "github.com/lib/pq"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+)
+
+func setupDB(t *testing.T) (*sql.DB, error) {
+	postgresContainer, err := postgres.Run(context.Background(),
+		"postgres:16-alpine",
+		postgres.WithDatabase("test"),
+		postgres.WithUsername("user"),
+		postgres.WithPassword("password"),
+		postgres.BasicWaitStrategies(),
+	)
+
+	if err != nil {
+		t.Fatalf("Failed to start PostgreSQL container: %v", err)
+		return nil, err
+	}
+	defer postgresContainer.Terminate(t.Context())
+
+	host, err := postgresContainer.Host(t.Context())
+	if err != nil {
+		t.Fatalf("Failed to get container host: %v", err)
+		return nil, err
+	}
+
+	port, err := postgresContainer.MappedPort(t.Context(), "5432")
+	if err != nil {
+		t.Fatalf("Failed to get mapped port: %v", err)
+		return nil, err
+	}
+
+	db, err := NewDB(host, port.Int(), "user", "password", "test")
+	if err != nil {
+		t.Fatalf("Failed to connect to database: %v", err)
+		return nil, err
+	}
+	defer db.Close()
+
+	err = CreateTable(db)
+	if err != nil {
+		t.Fatalf("Failed to create table: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func TestInsertTable(t *testing.T) {
+	db, err := setupDB(t)
+	if err != nil {
+		t.Fatalf("Failed to setup database: %v", err)
+		return
+	}
+
+	content := "Hello, Testcontainers!"
+	err = InsertPost(db, content)
+	if err != nil {
+		t.Fatalf("Failed to insert post: %v", err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,71 @@
+package poctestcontainers
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const ()
+
+func main() {
+	// connection string
+	host := "localhost"
+	port := 5432
+	user := "postgres"
+	password := "<password>"
+	dbname := "<dbname>"
+
+	db, err := NewDB(host, port, user, password, dbname)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	defer db.Close()
+
+	err = CreateTable(db)
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+
+}
+
+func NewDB(host string, port int, user, password, dbname string) (*sql.DB, error) {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		return nil, fmt.Errorf("error opening database: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	log.Println("Connected!")
+	return db, nil
+}
+
+func CreateTable(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS posts (
+		id SERIAL PRIMARY KEY,
+		content TEXT NOT NULL
+	);`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		return fmt.Errorf("error creating table: %w", err)
+	}
+
+	log.Println("Table created successfully")
+	return nil
+}
+
+func InsertPost(db *sql.DB, content string) error {
+	query := `INSERT INTO posts (content) VALUES ($1);`
+	_, err := db.Exec(query, content)
+	if err != nil {
+		return fmt.Errorf("error inserting post: %w", err)
+	}
+
+	log.Println("Post inserted successfully")
+	return nil
+}
